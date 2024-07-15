ddf06b2ef59e3f881683f4d7bf2d4361c20bebb6
Nicole <your@email.comnicoleokamoto@icloud.com>
2024-07-15 14:30:24 -0500

    Fixed products sorting to sort by type

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -1,10 +1,14 @@
 class ProductCategoriesController < ApplicationController
   def index
-    @product_category = ProductCategory.all
+    @product_type = ProductCategory.joins(:makeup_products)
+                                   .distinct
+                                   .order(:product_type)
+                                   .pluck(:product_type)
   end
 
   def show
-    @product_category = ProductCategory.find(params[:id])
-    @makeup_product = @product_category.makeup_products
+    product_type = params[:id]
+    @product_category = ProductCategory.where("product_type = ? OR product_category = ?", product_type, product_type).first
+    @makeup_products = MakeupProduct.joins(:product_category).where("product_categories.product_type = ? OR product_categories.product_category = ?", product_type, product_type)
   end
 end
