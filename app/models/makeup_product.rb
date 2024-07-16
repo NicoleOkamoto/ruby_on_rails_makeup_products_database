49024de0dc88fe06ef67e56f904d7b1a6c470673
Nicole <your@email.comnicoleokamoto@icloud.com>
2024-07-15 19:56:52 -0500

    Applied prettier

--- app/models/makeup_product.rb
+++ app/models/makeup_product.rb
@@ -3,7 +3,6 @@ class MakeupProduct < ApplicationRecord
   belongs_to :brand
 
   validates :product_name, presence: true
-  validates :product_type, presence: true
   validates :product_category_id, presence: true
   validates :brand_id, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
