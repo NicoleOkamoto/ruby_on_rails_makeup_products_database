f7cdbb96d55f6b720cdeb3e18b7ef1c134760200
Nicole <your@email.comnicoleokamoto@icloud.com>
2024-07-15 20:18:41 -0500

    Added search functionality

--- app/controllers/makeup_products_controller.rb
+++ app/controllers/makeup_products_controller.rb
@@ -1,9 +1,14 @@
 class MakeupProductsController < ApplicationController
   def index
-    @makeup_products = MakeupProduct.paginate(page: params[:page], per_page: 20)
+    if params[:search].present?
+      @makeup_products = MakeupProduct.where('product_name LIKE ?', "%#{params[:search]}%").paginate(page: params[:page], per_page: 20)
+    else
+      @makeup_products = MakeupProduct.paginate(page: params[:page], per_page: 20)
+    end
   end
 
   def show
     @makeup_product = MakeupProduct.find(params[:id])
   end
+
 end
